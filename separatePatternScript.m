c1ff78e67e2b415097715e8ce22bbb37cef72d42
ChengYangUmich <yangche@umich.edu>
2020-08-07 14:19:15 -0400

    fix typos

--- separatePatternScript.m
+++ separatePatternScript.m
@@ -4,9 +4,9 @@ close all;
 load xBOD;
 xDiurnal = zeros(size(xBOD));
 xLeachate = zeros(size(xBOD));
-parfor i= 1:size(xBOD,1)
+parfor i= 1%:size(xBOD,1)
     tic
-    [xDiurnal(i,:),xLeachate(i,:),~]=seperatePattern(xBOD(i,:)); 
+    [xDiurnal(i,:),xLeachate(i,:),~]=separatePattern(xBOD(i,:)); 
     disp(['Progressing ID = ', num2str(i)]);
     toc
 end
